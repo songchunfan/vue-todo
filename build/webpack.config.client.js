d8bab70e47e76c23504847cb3598538e28320106
songcf <1693402100@qq.com>
2019-02-11 09:52:23 +0800

    vue-loader配置

--- build/webpack.config.client.js
+++ build/webpack.config.client.js
@@ -35,7 +35,7 @@ if (isDev) {
         {
           test: /\.styl/,
           use: [
-            'style-loader',
+            'vue-style-loader',
             'css-loader',
             {
               loader: 'postcss-loader',
@@ -69,7 +69,7 @@ if (isDev) {
         {
           test: /\.styl/,
           use: ExtractPlugin.extract({
-            fallback: 'style-loader',
+            fallback: 'vue-style-loader',
             use: [
               'css-loader',
               {
--- client/layout/header.vue
+++ client/layout/header.vue
@@ -10,6 +10,7 @@
   h1{
     font-size 100px
     color: rgba(175, 47, 47, 0.4)
+    // color: rgba(255, 255, 0, 1)
     font-weight 100
     margin 20px
   }
